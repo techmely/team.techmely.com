5f7b31e56ecc7b04cd925f1a4732be648bb026a8
Harry Tran <nhattq.coding@gmail.com>
2023-07-20 07:19:49 +0700

    :star: init project

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,19 @@ import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { macroPlugin } from "@builder.io/vite-plugin-macro";
+import { partytownVite } from "@builder.io/partytown/utils";
+import { join } from "path";
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [
+      macroPlugin({ preset: "pandacss" }),
+      qwikCity(),
+      qwikVite(),
+      tsconfigPaths(),
+      partytownVite({ dest: join(__dirname, "dist", "~partytown") }),
+    ],
     preview: {
       headers: {
         "Cache-Control": "public, max-age=600",
