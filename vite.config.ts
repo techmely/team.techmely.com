7bd4793792f799a1430ff756fdf53a2ec6ebb457
Harry Tran <nhattq.coding@gmail.com>
2023-08-28 10:08:51 +0700

    chore: upgrade deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,25 @@
-import { defineConfig } from 'vite'
-import { qwikVite } from '@builder.io/qwik/optimizer'
-import { qwikCity } from '@builder.io/qwik-city/vite'
-import ViteTsconfigPaths from 'vite-tsconfig-paths'
-import { macroPlugin } from '@builder.io/vite-plugin-macro'
-import { partytownVite } from '@builder.io/partytown/utils'
-import ViteLightningCss from 'vite-plugin-lightningcss'
+import { defineConfig } from "vite";
+import { qwikVite } from "@builder.io/qwik/optimizer";
+import { qwikCity } from "@builder.io/qwik-city/vite";
+import ViteTsconfigPaths from "vite-tsconfig-paths";
+import { partytownVite } from "@builder.io/partytown/utils";
+import ViteLightningCss from "vite-plugin-lightningcss";
 
-import { join } from 'path'
+import { join } from "path";
 
 export default defineConfig(() => {
   return {
     plugins: [
-      macroPlugin({ preset: 'pandacss' }),
       qwikCity(),
       qwikVite(),
       ViteTsconfigPaths(),
-      partytownVite({ dest: join(__dirname, 'dist', '~partytown') }),
+      partytownVite({ dest: join(__dirname, "dist", "~partytown") }),
       ViteLightningCss(),
     ],
     preview: {
       headers: {
-        'Cache-Control': 'public, max-age=600',
+        "Cache-Control": "public, max-age=600",
       },
     },
-  }
-})
+  };
+});
